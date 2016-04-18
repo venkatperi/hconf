d270d80b80f5b8e81d0e24247d35452454640978
Venkat Peri <venkatperi@gmail.com>
2016-04-17 20:10:26 -0400

    removed dist/ from git and added npmignore

--- dist/lib/conventions/custom.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(opts) {
-  return {
-    name: opts.pkg.name,
-    description: opts.description || "custom file",
-    type: opts.type || 'file',
-    file: opts.file || opts.filename
-  };
-};
